4466cb7264cb0acbbf6170a77028d24275b4a44e
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-12-19 12:14:11 -0600

    base setup

---
+++ plza-fe/src/components/review/review.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Rating from "react-rating";
+
+//base rating setup without extra settings
+<Rating
+  emptySymbol="fa fa-star-o fa-2x"
+  fullSymbol="fa fa-star fa-2x"
+/>
